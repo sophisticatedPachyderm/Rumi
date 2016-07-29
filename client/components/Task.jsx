432299720d5e2aa4b17cd7c579da2cf2623ba09b
Steven Chung <steven.chung1@gmail.com>
2016-07-29 12:21:48 -0700

    I love CSS, so I've spent some time with Canvas HTML5.

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Paper from 'material-ui/Paper';
 import { connect } from 'react-redux';
 import { completeTaskToServer } from '../actions/taskActions';
 
@@ -15,11 +14,55 @@ let style = {
 class Task extends React.Component {
   constructor(props) {
     super(props);
+    console.log('Task props:', props);
+  }
+
+  roundRect(ctx, x, y, width, height, radius, fill, stroke) {
+    if (typeof stroke == 'undefined') {
+      stroke = true;
+    }
+    if (typeof radius === 'undefined') {
+      radius = 5;
+    }
+    if (typeof radius === 'number') {
+      radius = {tl: radius, tr: radius, br: radius, bl: radius};
+    } else {
+      var defaultRadius = {tl: 0, tr: 0, br: 0, bl: 0};
+      for (var side in defaultRadius) {
+        radius[side] = radius[side] || defaultRadius[side];
+      }
+    }
+    ctx.beginPath();
+    ctx.moveTo(x + radius.tl, y);
+    ctx.lineTo(x + width - radius.tr, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius.tr);
+    ctx.lineTo(x + width, y + height - radius.br);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius.br, y + height);
+    ctx.lineTo(x + radius.bl, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius.bl);
+    ctx.lineTo(x, y + radius.tl);
+    ctx.quadraticCurveTo(x, y, x + radius.tl, y);
+    ctx.closePath();
+    if (fill) {
+      ctx.fillStyle = fill;
+      ctx.fill();
+    }
+    if (stroke) {
+      ctx.stroke();
+    }
   }
 
   completeTask () {
     let taskId = this.props.id;
     this.props.dispatch(completeTaskToServer({taskId}));
+  }
+
+  componentDidMount() {
+    const ctx = this.refs.canvas.getContext('2d');
+    var fill = '#66ff66';
+    this.roundRect(ctx, 0, 0, 108, 108, 6, fill);
+    const bottomY = 72;
+    ctx.clearRect(0, 0, 108, bottomY);
   }
 
   render() {
@@ -35,21 +78,17 @@ class Task extends React.Component {
 
     return (
       <div>
-        <Paper
-          style={style2}
-          zDepth={3}
-          circle={true}
-          onTouchTap={this.completeTask.bind(this)}
-        >
-          <div className="innerTaskText">
-            {this.props.name}
-          </div>
-        </Paper>
+        <div className="outerTaskBox">
+            <canvas ref="canvas" width={108} height={108} />
+            <div className="innerTaskText">
+              {this.props.name}
+            </div>
+        </div>
       </div>
     );
   }
 }
 
-Task = connect()(Task);
-
+// Task = connect()(Task);
+// onTouchTap={this.completeTask.bind(this)}
 export default Task;
